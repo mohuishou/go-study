506b314ec52321e5043df2c9ed369dfcacea8b86
mohuishou <1@lailin.xyz>
2016-09-17 16:29:36 +0800

    update

--- ch1/question/q3.go
+++ ch1/question/q3.go
@@ -1,10 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	var a string
-	fmt.Scan(&a)
-	fmt.Println("您输入的是：" + a)
-	fmt.Scan(&a)
+	// q3_1()
+	// q3_2()
+	// q3_3()
+	q3_4()
+}
+
+func q3_1() {
+	k := 0
+I:
+	for i := 0; i < 100; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Print("A")
+			if k > 100 {
+				break I
+			}
+			k++
+		}
+		fmt.Println("")
+	}
+}
+
+func q3_2() {
+	str := "asSASA ddd 啊带伞dsjkdsjs dk"
+	count := utf8.RuneCount([]byte(str))
+	strRune := []rune(str)
+	bCount := 0
+
+	for i := 0; i < count; i++ {
+		bCount += utf8.RuneLen(strRune[i])
+	}
+	fmt.Println("字符串的字符数为：", count)
+	fmt.Println("字符串的字节数为：", bCount)
+}
+
+func q3_3() {
+	str := "asSASA ddd 啊带伞dsjkdsjs dk"
+	strRune := []rune(str)
+	strRep := []rune("abc")
+	for i := 0; i < 3; i++ {
+		strRune[3+i] = strRep[i]
+	}
+	strNew := string(strRune)
+	fmt.Print(strNew)
+
+}
+
+func q3_4() {
+	str := "foobar"
+	strRune := []rune(str)
+	strNew := ""
+	for i := utf8.RuneCountInString(str) - 1; i >= 0; i-- {
+		strNew += string(strRune[i])
+	}
+	fmt.Println(strNew)
 }
