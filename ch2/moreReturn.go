aba7a4566e7d11467f5d24fdf85299543ea23342
mohuishou <1@lailin.xyz>
2016-09-18 15:17:00 +0800

    update

---
+++ ch2/moreReturn.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	y, err := test(10)
+	if err != nil {
+		println("有一个错误")
+	}
+	println(y)
+}
+
+func test(x int) (y int, err error) {
+
+	y = x + 1
+	return y, err
+}
